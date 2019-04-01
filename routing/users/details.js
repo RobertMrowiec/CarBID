44face78fbe52c1d4b716c0f5d674cf75c058110
Robert <rmrowiec@icloud.com>
2019-04-01 23:38:29 +0200

    missed in last commit, joi validation

--- routing/users/details.js
+++ routing/users/details.js
@@ -1,5 +1,6 @@
 const User = require('../../models/User')
 const { defaultResponse } = require('../common')
+const { userValidate } = require('../../validation/user')
 
 exports.get = defaultResponse(() => User.find())
 
@@ -9,7 +10,11 @@ exports.pagination = defaultResponse(req => {
     const limit = Number(req.params.limit)
     return User.find().skip(limit * (req.params.page - 1)).limit(limit)
 })
-exports.add = defaultResponse(req => new User(req.body).save())
+
+exports.add = defaultResponse(async req => {
+    const result = await userValidate(req.body)
+    return !result.length ? new User(req.body).save() : result
+})
 
 exports.update = defaultResponse(req => User.findByIdAndUpdate(req.params.id, req.body, {new: true}))
 
---
+++ validation/user.js
@@ -0,0 +1,12 @@
+const Joi = require('joi');
+
+const userValidate = (body) => {
+    const UserSchema = Joi.object().keys({
+        password: Joi.string().regex(/^[a-zA-Z0-9]{6,}/),
+        email: Joi.string().regex(/([a-z][a-zA-Z0-9.-])\w+[@]+(herecars.com)/)
+    }).unknown()
+     
+    return Joi.validate(body, UserSchema).then(x => x).catch(validationError => validationError.details.map(d => d.message))
+}
+
+module.exports.userValidate = userValidate
